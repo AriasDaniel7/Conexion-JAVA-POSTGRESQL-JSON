877c8e8b764460c04c1cd4a1a1950b8599a0f2c7
Daniel Arias <daniel7arias2003@gmail.com>
2022-11-26 21:28:21 -0500

    Creacion del proyecto

---
+++ src/conexion_bd_con_json/Conexion_BD_con_JSON.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion_bd_con_json;
+
+/**
+ *
+ * @author Daniel Arias
+ */
+public class Conexion_BD_con_JSON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
